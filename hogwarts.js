32f7ae064632ddf19f3619c4439174d41255d153
Stephen <stephen.s.espinosa@gmail.com>
2021-09-23 14:34:24 +0200

    js adding hackmode

--- hogwarts.js
+++ hogwarts.js
@@ -720,3 +720,30 @@ function toggleModal(student) {
     document.querySelector(".main-container").classList.remove("disable");
   }
 }
+
+// ***********search bar************
+
+function searchBar() {
+  var input, filter, found, table, tr, td, i, j;
+  input = document.getElementById("searchInput");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("list");
+  tr = table.getElementsByTagName("tr");
+
+  // **loop rows***
+  for (i = 1; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td");
+    // ****loop column***
+    for (j = 0; j < td.length; j++) {
+      if (td[j].innerHTML.toUpperCase().indexOf(filter) > -1) {
+        found = true;
+      }
+    }
+    if (found) {
+      tr[i].style.display = "";
+      found = false;
+    } else {
+      tr[i].style.display = "none";
+    }
+  }
+}
